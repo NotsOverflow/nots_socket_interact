deb1dd3d5dc60c2cdf3f3763551643c373fe5fc2
notsgnik <labruillere@gmail.com>
2013-12-28 22:40:52 +0100

    updated

--- nots_socket.py
+++ nots_socket.py
@@ -3,12 +3,12 @@
 
 import sys, socket, time
 
-sleep_time = 1
-file_to_save = ""
+sleep_time = 0
+file_to_save = "file.txt"
 
 class mysocket:
 
-	def __init__(self,ip = None ,port = None, sock=None,timeout=0.1):
+	def __init__(self,ip = None ,port = None, sock=None,timeout=1):
 		if sock is None:
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		else:
@@ -94,6 +94,17 @@ def append_to_file(toappend):
 	f.write(toappend+"\n")
 	f.close()
 
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
 
 if __name__ == "__main__":
 
@@ -103,6 +114,7 @@ if __name__ == "__main__":
 		connection = mysocket('127.0.0.1',8888)
 		
 	if connection.connection:
+		set_keepalive_linux(connection.sock)
 		#print "timeout : '" + str(connection.sock.gettimeout()) + "'"
 		#print connection.comunicate()
 		connection.comunicate()
